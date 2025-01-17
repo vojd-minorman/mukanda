a53d7f9b83ff6395648c3d7f55bc47f255522f12
Vojdminorman <mitshneville7@gmail.com>
2025-01-17 23:15:22 +0100

    Augmentation padding-bottom au niveau de DocumentGrid

--- src/components/DocumentGrid.tsx
+++ src/components/DocumentGrid.tsx
@@ -87,7 +87,7 @@ export default function DocumentGrid({
           </Card>
         ))}
       </div>
-      <div className="mt-4 flex items-center justify-between">
+      <div className="mt-4 flex items-center justify-between pb-20">
         <p>Affichage de {startIndex + 1}-{Math.min(endIndex, sortedDocuments.length)} sur {sortedDocuments.length} documents</p>
         <div className="flex gap-2">
           <Button
